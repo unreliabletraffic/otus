977d5ebacb2599a137032e21bb5e4501c7a0c777
Artem Danilin <a.danilin@telekarta.tv>
2022-04-08 11:57:48 +0300

    Added condition with string length check

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,7 +9,8 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	str := ""
+	var str string
+
 	if s == "" {
 		return "", nil
 	}
@@ -17,17 +18,19 @@ func Unpack(s string) (string, error) {
 		if unicode.IsDigit(res) && i == 0 {
 			return "", ErrInvalidString
 		}
-		if unicode.IsDigit(res) && unicode.IsDigit(rune(s[i+1])) {
+		if unicode.IsDigit(res) && unicode.IsDigit(rune(s[i-1])) {
 			return "", ErrInvalidString
 		}
-		if unicode.IsDigit(res) && string(s[i]) == "0" {
+		if unicode.IsDigit(res) && s[i] == 48 { //Zero character check, rune 48 == string 0
 			str = str[:len(str)-1]
+			continue
 		}
-		if unicode.IsDigit(res) && s[i-2] == '\\' {
-			str += strings.Repeat(string(s[i-2])+string(s[i-1]), int(res-'0')-1)
+		if unicode.IsDigit(res) && len(str) > 3 && s[i-2] == '\\' {
+			str += strings.Repeat(string(s[i-2])+string(s[i-1]), int(res-'0'))
 			continue
 		} else if unicode.IsDigit(res) {
 			str += strings.Repeat(string(s[i-1]), int(res-'0')-1)
+			continue
 		}
 		str += string(s[i])
 	}
